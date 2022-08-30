6a7600c2f4874795bb1618c11bb831fcd34ceea5
Jyuston <justinyuen2000@hotmail.com>
2022-08-30 17:36:31 +1000

    fix: UI Index Improvements

--- src/components/Icons/IconWrapper.tsx
+++ src/components/Icons/IconWrapper.tsx
@@ -11,7 +11,7 @@ const IconWrapper: React.FC<IconWrapperProps> = ({ children, bgColour }) => {
     <div
       className={classNames(
         bgColour,
-        "flex min-w-[120px] items-center justify-center gap-2 rounded-full px-4 py-2 text-sm font-medium text-slate-200 transition duration-300 ease-in-out will-change-transform hover:scale-110 hover:cursor-pointer md:px-8 md:py-2 md:text-lg"
+        "flex min-w-[120px] items-center justify-center gap-2 rounded-full px-4 py-2 text-sm font-medium text-slate-200 transition duration-300 ease-in-out will-change-transform hover:scale-110 hover:cursor-pointer hover:drop-shadow-xl md:px-8 md:py-2 md:text-lg"
       )}
     >
       {children}
